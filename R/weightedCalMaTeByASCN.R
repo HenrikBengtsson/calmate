9199b368810918bddbcf603cf05c78f273b43638
Henrik Bengtsson <hb@aroma-project.org>
2010-05-18 13:57:48 +0000

    Version: 0.2.0 [2010-05-18] o Added weightedCalMaTeByASCN() which takes an Jx2xI array   of allele-specific signals, where J is the number of SNPs,   2 is the number of alleles, and I is the number of samples.
    Version: 0.1.2 [2010-05-18]
    o CLEAN UP: Source code cleanup and standardization.
    o ROBUSTNESS: Added a redundancy test.

---
+++ R/weightedCalMaTeByASCN.R
@@ -0,0 +1,112 @@
+# @title "Internal CalMaTe fit function"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @usage
+#
+# \arguments{
+#  \item{data}{An Jx2xI @numeric array, where J is the number of SNPs,
+#          2 is the number of alleles, and I is the number of samples.}
+#  \item{...}{Additional arguments passed to internal 
+#          \code{CalMaTeWeighted().}
+#  \item{verbose}{See @see "R.utils::Verbose".}
+# }
+#
+# \value{
+#   Returns an Jx2xI @numeric array.
+# }
+#
+setMethodS3("weightedCalMaTeByASCN", "array", function(data, ..., verbose=FALSE) {
+  # Argument 'data':
+  if (!is.array(data)) {
+    throw("Argument 'data' is not an array: ", class(data)[1]);
+  }
+  dim <- dim(data);
+  if (length(dim) != 3) {
+    throw("Argument 'data' is not a 3-dimensional array: ", 
+                                                paste(dim, collapse="x"));
+  }
+  if (dim[2] != 2) {
+    throw("Argument 'data' is not a Jx2xI-dimensional array: ", 
+                                                paste(dim, collapse="x"));
+  }
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+
+
+  verbose && enter(verbose, "weightedCalMaTeByASCN()");
+  verbose && cat(verbose, "ASCN signals:");
+  verbose && str(verbose, data);
+
+  dim <- dim(data);
+  dimnames <- dimnames(data);
+
+
+  verbose && enter(verbose, "Identifying non-finite data points");
+  # Keep finite values
+  ok <- (is.finite(data[,"A",]) & is.finite(data[,"B",]));
+  ok <- rowAlls(ok);
+  verbose && summary(verbose, ok);
+  hasNonFinite <- any(!ok);
+  if (hasNonFinite) {
+    verbose && enter(verbose, "Excluding non-finite data points");
+    dataS <- dataS[ok,,,drop=FALSE];
+    verbose && str(verbose, data);
+    verbose && exit(verbose);
+  } else {
+    verbose && cat(verbose, "All data points are finite.");
+    dataS <- data;
+  }
+  verbose && exit(verbose);
+
+  verbose && enter(verbose, "Fitting CalMaTe");
+  dataA <- dataS[,1,,drop=FALSE];
+  dataB <- dataS[,2,,drop=FALSE];
+  dim(dataA) <- dim[-2];
+  dim(dataB) <- dim[-2];
+  rm(dataS);
+  dataT <- CalMaTeWeighted(dataA, dataB, ...);
+  verbose && str(verbose, head(dataT));
+  rm(dataA, dataB);
+  verbose && exit(verbose);
+
+  verbose && enter(verbose, "Restructuring into an array");
+  # There is one element per SNP
+  dimT <- c(dim(dataT[[1]]), length(dataT));
+  dataT <- unlist(dataT, use.names=FALSE);
+  dim(dataT) <- dimT;
+  dataT <- aperm(dataT, perm=c(3,1,2));
+  verbose && str(verbose, dataT);
+  verbose && exit(verbose);
+
+  if (hasNonFinite) {
+    verbose && enter(verbose, "Expanding to array with non-finite");
+    dataC <- data;
+    dataC[ok,,] <- dataT;
+    verbose && str(verbose, dataC);
+    verbose && exit(verbose);
+  } else {
+    dataC <- dataT;
+  }
+  rm(dataT);
+
+  # Sanity check
+  stopifnot(identical(dim(dataC), dim(data)));
+
+  verbose && cat(verbose, "Calibrated ASCN signals:");
+  verbose && str(verbose, dataC);
+
+  verbose && exit(verbose);
+
+  dataC;
+}) # weightedCalMaTeByASCN()
+
+
+###########################################################################
+# HISTORY:
+# 2010-05-18
+# o Created.
+###########################################################################
--- inst/testScripts/system/chipTypes/Mapping250K_Nsp,Sty/20100518b,CalMaTe.R
+++ inst/testScripts/system/chipTypes/Mapping250K_Nsp,Sty/20100518b,CalMaTe.R
@@ -31,7 +31,7 @@ stopifnot(identical(getNames(dsT), getNames(dsB)));
 # Study array #1 and chromosome 22
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 array <- 1;
-chr <- 1;
+chr <- 17;
 
 chrTag <- sprintf("Chr%02d", chr);
 
@@ -46,44 +46,43 @@ units <- getUnitsOnChromosome(ugp, chr);
 pos <- getPositions(ugp, units=units);
 
 # Extract (total, fracB) signals
-theta <- extractMatrix(dfT, units=units, drop=TRUE);
-beta <- extractMatrix(dfB, units=units, drop=TRUE);
+theta <- extractMatrix(dsT, units=units);
+beta <- extractMatrix(dsB, units=units);
 
 # Convert to allele-specific SNP signals (thetaA, thetaB)
 thetaB <- theta * beta;
 thetaA <- theta - thetaB;
-thetaAB <- matrix(c(thetaA, thetaB), ncol=2, byrow=FALSE);
-colnames(thetaAB) <- c("A", "B");
-rm(thetaA, thetaB);
+dimnames <- c(dimnames(thetaA), list(c("A", "B")));
+dim <- c(dim(thetaA), 2);
+thetaAB <- array(c(thetaA, thetaB), dim=dim, dimnames=dimnames);
+thetaAB <- aperm(thetaAB, perm=c(1,3,2));
+rm(thetaA, thetaB, dim, dimnames);
 
-# Keep non-missing values
-idxs <- which(is.finite(thetaAB[,"A"]) & is.finite(thetaAB[,"B"]));
-res <- CalMaTeWeighted(thetaAB[idxs,"A"], thetaAB[idxs,"B"]);
-res <- lapply(res, FUN=function(x) {
-  x <- t(x);
-  colnames(x) <- c("A", "B");
-  x;
-});
-naValue <- as.double(NA);
-thetaABC <- matrix(naValue, nrow=nrow(thetaAB), ncol=2);
-colnames(thetaABC) <- c("A", "B");
-thetaABC[idxs,] <- res[[1]];
-thetaC <- thetaABC[,"A"] + thetaABC[,"B"];
-betaC <- thetaABC[,"B"] / thetaC;
+res <- weightedCalMaTeByASCN(thetaAB, verbose=-8);
+
+  
+CC <- data;
+
+thetaC <- CC[,"A",] + CC[,"B",];
+betaC <- CC[,"B",] / thetaC;
 
 
 # Plot
 ylim <- c(-0.2, 1.2);
 subplots(2, ncol=1);
 
-fracB <- RawAlleleBFractions(beta, x=pos);
+# Array #1
+ii <- 1;
+name <- colnames(beta)[ii];
+
+fracB <- RawAlleleBFractions(beta[,ii], x=pos);
 plot(fracB, pch=".", ylim=ylim);
-stext(side=3, pos=0, getName(dfT));
+stext(side=3, pos=0, name);
 stext(side=3, pos=1, chrTag);
 
-fracBC <- RawAlleleBFractions(betaC, x=pos);
+fracBC <- RawAlleleBFractions(betaC[,ii], x=pos);
 plot(fracBC, pch=".", ylim=ylim);
-stext(side=3, pos=0, getName(dfT));
+stext(side=3, pos=0, name);
 stext(side=3, pos=1, chrTag);
 
 
